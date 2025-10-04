ad3a76a6d2b42c9eb1ef95c69ec3e89debdc84a7
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-10-04 16:03:01 -0400

    add some docs

--- hxhardware/CPU.hx
+++ hxhardware/CPU.hx
@@ -2,21 +2,43 @@ package hxhardware;
 
 import cpp.SizeT;
 
+/**
+ * Get information about the CPU.
+ */
 @:include("cpu.hpp")
 @:buildXml("<include name='${haxelib:hxhardware}/build.xml' />")
 extern class CPU {
+	/**
+	 * Should be called before attempting to get any values.
+	 */
 	@:native("hxhardware::init")
 	static function init():Void;
 
+	/**
+	 * Gets the total CPU usage of the system.
+	 * @return The usage (as a %).
+	 */
 	@:native("hxhardware::getSystemTotalCPUUsage")
 	static function getSystemTotalCPUUsage():Float;
 
+	/**
+	 * Gets the peak CPU usage of the system.
+	 * @return The usage (as a %).
+	 */
 	@:native("hxhardware::getSystemTotalPeakCPUUsage")
 	static function getSystemTotalPeakCPUUsage():Float;
 
+	/**
+	 * Gets the current CPU usage of the current process.
+	 * @return The usage (as a %).
+	 */
 	@:native("hxhardware::getProcessCPUUsage")
 	static function getProcessCPUUsage():Float;
 
+	/**
+	 * Gets the peak CPU usage of the current process.
+	 * @return The usage (as a %).
+	 */
 	@:native("hxhardware::getProcessPeakCPUUsage")
 	static function getProcessPeakCPUUsage():Float;
 }
